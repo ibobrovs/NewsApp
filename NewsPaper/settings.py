f66228d1db99b8b4a8f1c4122186cbac47a3bca3
Ivan Bobrov <ibobrovs9@gmail.com>
2025-01-11 12:50:35 +0200

    import logging

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -158,7 +158,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = "bobrovs.i@yandex.com"
-EMAIL_HOST_PASSWORD = "iliezvcovrxqizez"
+EMAIL_HOST_PASSWORD = "opucdbmxqeuyuzkh"
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
--- news/management/commands/runapscheduler.py
+++ news/management/commands/runapscheduler.py
@@ -1,23 +1,44 @@
 import logging
+from datetime import timedelta
+from django.utils.timezone import now
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
-from django.core.mail import mail_managers
+from django.core.mail import EmailMultiAlternatives
 from django.core.management.base import BaseCommand
 from django_apscheduler import util
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
-from news.models import Post
+from news.models import Post, Category, User
 
 logger = logging.getLogger(__name__)
 
 
-def my_job():
-    posts = Post.objects.order_by('price')[:3]
-    text = '\n'.join(['{} - {}'.format(p.name, p.price) for p in posts])
-    mail_managers("Самые дешевые товары", text)
+def send_weekly_articles():
+    last_week = now() - timedelta(days=7)
+    recent_posts = Post.objects.filter(dateCreation__gte=last_week)
+    categories = Category.objects.filter(post__in=recent_posts).distinct()
+
+    for category in categories:
+        posts_in_category = recent_posts.filter(postCategory=category)
+        post_list = "\n".join(
+            [f"- {post.title}: http://127.0.0.1:8000{post.get_absolute_url()}" for post in posts_in_category]
+        )
+        subscribers = User.objects.filter(subscriptions__category=category).values_list('email', flat=True)
+        subject = f'Новые статьи в категории "{category.name}" за неделю'
+        text_content = f'Список новых статей:\n{post_list}'
+        html_content = f'<p>Список новых статей:</p><ul>' + "".join(
+            [f'<li><a href="http://127.0.0.1:8000{post.get_absolute_url()}">{post.title}</a></li>' for post in posts_in_category]
+        ) + '</ul>'
+
+        for email in subscribers:
+            msg = EmailMultiAlternatives(subject, text_content, None, [email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+    logger.info("Weekly articles email sent successfully.")
 
 
 @util.close_old_connections
@@ -33,19 +54,17 @@ class Command(BaseCommand):
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
         scheduler.add_job(
-            my_job,
-            trigger=CronTrigger(minute="20", hour="10"),
-            id="my_job",  # The `id` assigned to each job MUST be unique
+            send_weekly_articles,
+            trigger=CronTrigger(day_of_week="fri", hour=18, minute=0),
+            id="send_weekly_articles",
             max_instances=1,
             replace_existing=True,
         )
-        logger.info("Added job 'my_job'.")
+        logger.info("Added job 'send_weekly_articles'.")
 
         scheduler.add_job(
             delete_old_job_executions,
-            trigger=CronTrigger(
-                day_of_week="mon", hour="00", minute="00"
-            ),
+            trigger=CronTrigger(day_of_week="mon", hour=0, minute=0),
             id="delete_old_job_executions",
             max_instances=1,
             replace_existing=True,
@@ -58,4 +77,4 @@ class Command(BaseCommand):
         except KeyboardInterrupt:
             logger.info("Stopping scheduler...")
             scheduler.shutdown()
-            logger.info("Scheduler shut down successfully!")
+            logger.info("Scheduler shut down successfully.")
